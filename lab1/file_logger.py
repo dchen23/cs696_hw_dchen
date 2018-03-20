0897812654b93e0725eaf979ac54900dfabbdc9c
Dingyi Chen <dchen23@dons.usfca.edu>
2018-03-20 14:28:29 -0700

    add ignore

--- lab1/file_logger.py
+++ lab1/file_logger.py
@@ -2,7 +2,14 @@ from logger import logger
 
 class file_logger(logger):
 
+    def __init__(self, log_level):
+        logger.__init__(self, log_level)
+        self.__filename__ = 'file_log.txt'
+
+    def set_filename(self, name):
+        self.__filename__ = name
+
     def log(self, log_level, message):
         if (log_level <= self.__log_level__):
-            with open('file_log.txt', 'a') as f:
+            with open(self.__filename__, 'a') as f:
                 f.write("{}: {}\n".format(log_level, message))
